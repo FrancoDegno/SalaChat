76b3156e52a91bf17fd2f164f1fea3d092c29684
franco <francodegano@hotmail.com>
2018-09-12 14:30:38 -0300

    SalaChat

--- SalaChat/src/app/Components/room/room.component.ts
+++ SalaChat/src/app/Components/room/room.component.ts
@@ -11,20 +11,31 @@ export class RoomComponent{
 
   rooms:Sala[];
   actualRoom:Sala;
+  newSalaName:String;
   @Input() actualUserRoom:Usuario;
   @Output() sendRooms=new EventEmitter;
+  salaid:number=0;
 
   constructor() { 
     this.rooms=[];
-    let sala1 = new Sala(1,"public");
+    let sala1 = new Sala(this.salaid,"sala1");
+    sala1.addMsj(-1,"Sala1","");
     this.rooms.push(sala1);
 
-    let sala2 = new Sala(2,"public");
+    this.salaid++;
+    let sala2 = new Sala(this.salaid,"sala2");
+    sala2.addMsj(-1,"Sala2","");
+    this.salaid++;
     this.rooms.push(sala2);
 
   }
 
-
+  createRoom()
+  {
+    let sala = new Sala(this.salaid,this.newSalaName);
+    sala.addMsj(-1,sala.name,"");
+    this.rooms.push(sala);
+  }
 
   setActualRoom(actual:Sala)
   {
